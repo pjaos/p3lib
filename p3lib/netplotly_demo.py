771df6d1abe7ce370f9cfc1c3fca3c7182ab85bc
Paul Austen <pausten.os@gmail.com>
2020-09-07 07:00:12 +0100

    Add another example plot from https://plotly.com/python/subplots/

--- p3lib/netplotly_demo.py
+++ p3lib/netplotly_demo.py
@@ -74,10 +74,62 @@ class NetPlotlyDemo(object):
         self._netPlotly.save(fig, autoOpen=True)
         self._netPlotly.upload()
 
+    def subPlotsSharedAxis(self):
+        trace1 = go.Scatter(
+            x=[1, 2, 3],
+            y=[2, 3, 4]
+        )
+        trace2 = go.Scatter(
+            x=[20, 30, 40],
+            y=[5, 5, 5],
+            xaxis="x2",
+            yaxis="y"
+        )
+        trace3 = go.Scatter(
+            x=[2, 3, 4],
+            y=[600, 700, 800],
+            xaxis="x",
+            yaxis="y3"
+        )
+        trace4 = go.Scatter(
+            x=[4000, 5000, 6000],
+            y=[7000, 8000, 9000],
+            xaxis="x4",
+            yaxis="y4"
+        )
+        data = [trace1, trace2, trace3, trace4]
+        layout = go.Layout(
+            xaxis=dict(
+                domain=[0, 0.45]
+            ),
+            yaxis=dict(
+                domain=[0, 0.45]
+            ),
+            xaxis2=dict(
+                domain=[0.55, 1]
+            ),
+            xaxis4=dict(
+                domain=[0.55, 1],
+                anchor="y4"
+            ),
+            yaxis3=dict(
+                domain=[0.55, 1]
+            ),
+            yaxis4=dict(
+                domain=[0.55, 1],
+                anchor="x4"
+            )
+        )
+        fig = go.Figure(data=data, layout=layout)
+        fig.update_layout(title_text='Shared Axis subplots')
+        self._netPlotly.save(fig, autoOpen=True)
+        self._netPlotly.upload()
+
     def demo(self):
         """@brief show the demo of all features."""
         self.singleTimeSeries()
         self.threeTracesTwoPlot()
+        self.subPlotsSharedAxis()
 
     def listPlots(self):
         """@brief List the available plots."""
