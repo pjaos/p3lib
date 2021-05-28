e57ef6ad2fa480658645b3aa26345599786a3d70
Paul Austen <pjaos@gmail.com>
2021-05-28 09:39:11 +0100

    Bump version.

--- doc/html/search/variables_3.js
+++ doc/html/search/variables_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['file_5finput_737',['file_input',['../classbokeh__demo_1_1BokehDemoA.html#a7be426c75695ea9bb534064d55732475',1,'bokeh_demo::BokehDemoA']]],
-  ['filetosave_738',['fileToSave',['../classp3lib_1_1bokeh__gui_1_1TimeSeriesPlotter.html#a422629a82aca2100a444654cc00dca1a',1,'p3lib.bokeh_gui.TimeSeriesPlotter.fileToSave()'],['../classbokeh__demo_1_1BokehDemoA.html#a422629a82aca2100a444654cc00dca1a',1,'bokeh_demo.BokehDemoA.fileToSave()'],['../classbokeh__demo_1_1BokehDemoB.html#a422629a82aca2100a444654cc00dca1a',1,'bokeh_demo.BokehDemoB.fileToSave()']]],
+  ['filetosave_738',['fileToSave',['../classbokeh__demo_1_1BokehDemoA.html#a422629a82aca2100a444654cc00dca1a',1,'bokeh_demo.BokehDemoA.fileToSave()'],['../classbokeh__demo_1_1BokehDemoB.html#a422629a82aca2100a444654cc00dca1a',1,'bokeh_demo.BokehDemoB.fileToSave()']]],
   ['float_5fnumber_5ftype_739',['FLOAT_NUMBER_TYPE',['../classp3lib_1_1pconfig_1_1ConfigManager.html#a4491638bc484710aaef10cc2b5d8a089',1,'p3lib::pconfig::ConfigManager']]]
 ];
