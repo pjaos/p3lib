b7cac05a5a66999fb67686cb1ef8405850ae10e9
Paul Austen <pjaos@gmail.com>
2022-01-26 08:22:46 +0000

    Add examples of how to run the bokeh example code.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,3 +11,6 @@ echo "Open browser and display static graphs using plotly."
 python3.8 -m pipenv run netplotly_demo
 echo "Open browser and show real time plots using bokeh."
 python3.8 -m pipenv run time_series_plot_example
+# Other bokeh examples
+# ./run_bokeh_demo.sh 
+# python3.8 -m pipenv run python3 GUIModel_A_example.py
