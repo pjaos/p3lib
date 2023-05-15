25be5b5ae640eafe8d834987a875eba7eaa40ef9
Paul Austen <pjaos@gmail.com>
2023-05-15 06:49:39 +0100

    Update the documentation.

--- doc/html/search/functions_5.js
+++ doc/html/search/functions_5.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['edit_633',['edit',['../classp3lib_1_1pconfig_1_1ConfigManager.html#a5df088f121b2a6e12e8dbd9cbfc8e225',1,'p3lib::pconfig::ConfigManager']]],
   ['enabledebug_634',['enableDebug',['../classp3lib_1_1uio_1_1UIO.html#aaf00e13e29efe691aacc67d229c29349',1,'p3lib::uio::UIO']]],
-  ['enablesyslog_635',['enableSyslog',['../classp3lib_1_1uio_1_1UIO.html#a70c01a18249a90b0055bc6fe2e81e703',1,'p3lib::uio::UIO']]],
+  ['enablesyslog_635',['enableSyslog',['../classp3lib_1_1uio_1_1UIO.html#ad6c4e4e80e648af5064ac0a76c1bc02c',1,'p3lib::uio::UIO']]],
   ['encrypt_636',['Encrypt',['../classp3lib_1_1pconfig_1_1ConfigManager.html#ac8af7f07d4d874226ba936a94e324629',1,'p3lib::pconfig::ConfigManager']]],
   ['ensureboolint_637',['ensureBoolInt',['../namespacep3lib_1_1helper.html#adfe2cecd86a8b3f85ae467847ee238a3',1,'p3lib::helper']]],
   ['ensuretableexists_638',['ensureTableExists',['../classp3lib_1_1database__if_1_1DatabaseIF.html#a44ed3a19b9d85fb990978ed4308194e5',1,'p3lib::database_if::DatabaseIF']]],
@@ -11,5 +11,5 @@ var searchData=
   ['example1_641',['example1',['../namespacepconfig__example.html#a51795ebcc39b88d161ae1eff6b07bf8f',1,'pconfig_example']]],
   ['example2_642',['example2',['../namespacepconfig__example.html#a9b9e94273b0a42cd57fcce4797ea8f8b',1,'pconfig_example']]],
   ['example3_643',['example3',['../namespacepconfig__example.html#ad2749a073718f864a6b5fa90447f83a7',1,'pconfig_example']]],
-  ['executesql_644',['ExecuteSQL',['../classp3lib_1_1database__if_1_1DatabaseIF.html#a0b70c4a751cd2ccd735b8fa29517e6e0',1,'p3lib.database_if.DatabaseIF.ExecuteSQL(connection, cursor, cmd)'],['../classp3lib_1_1database__if_1_1DatabaseIF.html#a6ffd1a646b85c8ff98412b9082adcd15',1,'p3lib.database_if.DatabaseIF.executeSQL(self, sqlCmd)']]]
+  ['executesql_644',['executeSQL',['../classp3lib_1_1database__if_1_1DatabaseIF.html#a6ffd1a646b85c8ff98412b9082adcd15',1,'p3lib.database_if.DatabaseIF.executeSQL(self, sqlCmd)'],['../classp3lib_1_1database__if_1_1DatabaseIF.html#a0b70c4a751cd2ccd735b8fa29517e6e0',1,'p3lib.database_if.DatabaseIF.ExecuteSQL(connection, cursor, cmd)']]]
 ];
