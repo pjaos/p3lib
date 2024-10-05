0f09632b58a185a79bd6b714c400a4cf022a7fcb
Paul Austen <pjaos@gmail.com>
2024-10-05 10:21:11 +0100

    Bump to 1.1.82

--- doc/html/search/all_8.js
+++ doc/html/search/all_8.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['handle_0',['handle',['../classp3lib_1_1json__networking_1_1JsonServerHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.json_networking.JsonServerHandler.handle()'],['../classp3lib_1_1ssh_1_1ForwardingHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.ssh.ForwardingHandler.handle()'],['../classp3lib_1_1table__plot_1_1Table2DPlotServer_1_1ServerSessionHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.table_plot.Table2DPlotServer.ServerSessionHandler.handle()'],['../classtest__json__networking_1_1ServerSessionHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'test_json_networking.ServerSessionHandler.handle()']]],
-  ['hash_5ffile_1',['HASH_FILE',['../classbokeh__auth__test_1_1CredentialsHasherTester.html#a635e46dba52617450c6da748a2c182d7',1,'bokeh_auth_test::CredentialsHasherTester']]],
-  ['helper_2epy_2',['helper.py',['../helper_8py.html',1,'']]],
-  ['hexadecimal_5fint_5fnumber_5ftype_3',['HEXADECIMAL_INT_NUMBER_TYPE',['../classp3lib_1_1pconfig_1_1ConfigManager.html#a8692a265fefe6eba77dd0cf531eeb303',1,'p3lib::pconfig::ConfigManager']]],
-  ['host_4',['HOST',['../classtest__json__networking_1_1TestClass.html#a9753bdac85c26176cba869e2599824a3',1,'test_json_networking::TestClass']]],
-  ['html_5ffile_5',['HTML_FILE',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a9f941e8f3eff218d8370ae7f8baff475',1,'p3lib::table_plot::Table2DPlotServer']]],
-  ['htmlfile_6',['htmlFile',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a53463d910a7e8f74b47f9e4745d163f4',1,'p3lib::table_plot::StaticPlotParams']]]
+  ['handleoptions_1',['HandleOptions',['../classp3lib_1_1boot__manager_1_1BootManager.html#a6979217d6b41314f1d2ab9c51501580f',1,'p3lib::boot_manager::BootManager']]],
+  ['hash_5ffile_2',['HASH_FILE',['../classbokeh__auth__test_1_1CredentialsHasherTester.html#a635e46dba52617450c6da748a2c182d7',1,'bokeh_auth_test::CredentialsHasherTester']]],
+  ['helper_2epy_3',['helper.py',['../helper_8py.html',1,'']]],
+  ['hexadecimal_5fint_5fnumber_5ftype_4',['HEXADECIMAL_INT_NUMBER_TYPE',['../classp3lib_1_1pconfig_1_1ConfigManager.html#a8692a265fefe6eba77dd0cf531eeb303',1,'p3lib::pconfig::ConfigManager']]],
+  ['host_5',['HOST',['../classtest__json__networking_1_1TestClass.html#a9753bdac85c26176cba869e2599824a3',1,'test_json_networking::TestClass']]],
+  ['html_5ffile_6',['HTML_FILE',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a9f941e8f3eff218d8370ae7f8baff475',1,'p3lib::table_plot::Table2DPlotServer']]],
+  ['htmlfile_7',['htmlFile',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a53463d910a7e8f74b47f9e4745d163f4',1,'p3lib::table_plot::StaticPlotParams']]]
 ];
