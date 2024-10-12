3e30abffde11566f6dd1c42f5e97e64dcf984a74
Paul Austen <pjaos@gmail.com>
2024-10-12 09:35:50 +0100

    Bump to 1.1.88

--- doc/html/search/all_b.js
+++ doc/html/search/all_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['kern_0',['KERN',['../classp3lib_1_1uio_1_1FACILITY.html#afbf20c0a8b93ea7b6015e1c699555948',1,'p3lib::uio::FACILITY']]],
-  ['knob_5finput_5ffield_1',['KNOB_INPUT_FIELD',['../classp3lib_1_1ngt_1_1YesNoDialog.html#a46d5ca2e6c2ed7e8195abb8a34293c4c',1,'p3lib::ngt::YesNoDialog']]]
+  ['key_5fedit_5forder_5flist_1',['KEY_EDIT_ORDER_LIST',['../classp3lib_1_1pconfig_1_1DotConfigManager.html#a372d3ccef196db4379c823abc946ca48',1,'p3lib::pconfig::DotConfigManager']]],
+  ['knob_5finput_5ffield_2',['KNOB_INPUT_FIELD',['../classp3lib_1_1ngt_1_1YesNoDialog.html#a46d5ca2e6c2ed7e8195abb8a34293c4c',1,'p3lib::ngt::YesNoDialog']]]
 ];
