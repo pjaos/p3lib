477f4750e08ef606da492221118801a31caa5971
Paul Austen <pjaos@gmail.com>
2024-10-24 06:37:15 +0100

    Bump to 1.1.98

--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['check_5fpermission_0',['check_permission',['../classp3lib_1_1bokeh__auth_1_1LoginHandler.html#a5b3c0b392b496dae93d0358afc410ee5',1,'p3lib::bokeh_auth::LoginHandler']]],
-  ['checkautostartstatus_1',['CheckAutoStartStatus',['../classp3lib_1_1boot__manager_1_1BootManager.html#a5261fd00abf2de0fff708a966e95aa26',1,'p3lib::boot_manager::BootManager']]],
+  ['checkautostartstatus_1',['CheckAutoStartStatus',['../classp3lib_1_1boot__manager_1_1BootManager.html#ae2a5ea661f5d142d120158688de9f690',1,'p3lib::boot_manager::BootManager']]],
   ['checkport_2',['CheckPort',['../classp3lib_1_1ngt_1_1TabbedNiceGui.html#a1fd8f3da6216e1cf06513c82dca91f96',1,'p3lib::ngt::TabbedNiceGui']]],
   ['checktableexists_3',['checktableexists',['../classp3lib_1_1database__if_1_1DatabaseIF.html#a623d9144da9f518b975bfd228c90d4e5',1,'p3lib.database_if.DatabaseIF.checkTableExists(self, tableName)'],['../classp3lib_1_1database__if_1_1DatabaseIF.html#ae5b178013634c3361ae3f621b8d0f9f9',1,'p3lib.database_if.DatabaseIF.CheckTableExists(connection, tableName)']]],
   ['clearprogbar_4',['clearProgBar',['../classp3lib_1_1uio_1_1UIO.html#aba3cc594592fbcb867a76baf4f8239cb',1,'p3lib::uio::UIO']]],
@@ -12,5 +12,5 @@ var searchData=
   ['createdatabase_9',['createDatabase',['../classp3lib_1_1database__if_1_1DatabaseIF.html#ac5dde703e0bb84212ccdb08138a433c2',1,'p3lib::database_if::DatabaseIF']]],
   ['createnewplot_10',['createNewPlot',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a0215eccd733d29b15e179c482c532511',1,'p3lib::table_plot::Table2DPlotServer']]],
   ['createplot_11',['createplot',['../classp3lib_1_1bokeh__gui_1_1TimeSeriesPlotter.html#a810c8bf572d410e341e38f4dfe0a8137',1,'p3lib.bokeh_gui.TimeSeriesPlotter.createPlot()'],['../classbokeh__demo_1_1BokehDemoA.html#a810c8bf572d410e341e38f4dfe0a8137',1,'bokeh_demo.BokehDemoA.createPlot()'],['../classbokeh__demo_1_1BokehDemoB.html#a810c8bf572d410e341e38f4dfe0a8137',1,'bokeh_demo.BokehDemoB.createPlot()']]],
-  ['createtable_12',['createtable',['../classp3lib_1_1database__if_1_1DatabaseIF.html#a9cf2400908055da860f999f2b1914898',1,'p3lib.database_if.DatabaseIF.CreateTable(connection, tableName, tableSchemaDict)'],['../classp3lib_1_1database__if_1_1DatabaseIF.html#a89cdb892094a4ddd15b6b8b625255931',1,'p3lib.database_if.DatabaseIF.createTable(self, tableName, tableSchemaDict)']]]
+  ['createtable_12',['createtable',['../classp3lib_1_1database__if_1_1DatabaseIF.html#a89cdb892094a4ddd15b6b8b625255931',1,'p3lib.database_if.DatabaseIF.createTable(self, tableName, tableSchemaDict)'],['../classp3lib_1_1database__if_1_1DatabaseIF.html#a9cf2400908055da860f999f2b1914898',1,'p3lib.database_if.DatabaseIF.CreateTable(connection, tableName, tableSchemaDict)']]]
 ];
--- doc/html/search/functions_8.js
+++ doc/html/search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['handle_0',['handle',['../classp3lib_1_1json__networking_1_1JsonServerHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.json_networking.JsonServerHandler.handle()'],['../classp3lib_1_1ssh_1_1ForwardingHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.ssh.ForwardingHandler.handle()'],['../classp3lib_1_1table__plot_1_1Table2DPlotServer_1_1ServerSessionHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.table_plot.Table2DPlotServer.ServerSessionHandler.handle()'],['../classtest__json__networking_1_1ServerSessionHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'test_json_networking.ServerSessionHandler.handle()']]],
-  ['handleoptions_1',['HandleOptions',['../classp3lib_1_1boot__manager_1_1BootManager.html#a0ff0ea379ee79bc9422c28f51f638b0f',1,'p3lib::boot_manager::BootManager']]]
+  ['handleoptions_1',['HandleOptions',['../classp3lib_1_1boot__manager_1_1BootManager.html#aa9d674696f3249198e314f66784d0ba5',1,'p3lib::boot_manager::BootManager']]]
 ];
