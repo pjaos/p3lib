2bcbd9c7771c26c04684620dc6b487e2701ce3ff
Paul Austen <pjaos@gmail.com>
2025-07-07 07:04:07 +0100

    Bump to 1.1.126

--- doc/html/search/all_8.js
+++ doc/html/search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['handle_0',['handle',['../classp3lib_1_1json__networking_1_1JsonServerHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.json_networking.JsonServerHandler.handle()'],['../classp3lib_1_1ssh_1_1ForwardingHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.ssh.ForwardingHandler.handle()'],['../classp3lib_1_1table__plot_1_1Table2DPlotServer_1_1ServerSessionHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'p3lib.table_plot.Table2DPlotServer.ServerSessionHandler.handle()'],['../classtest__json__networking_1_1ServerSessionHandler.html#a900a6bb72b36cac786c3060914a1935f',1,'test_json_networking.ServerSessionHandler.handle()']]],
-  ['handle_5fdouble_5fclick_1',['handle_double_click',['../classp3lib_1_1ngt_1_1local__file__picker.html#a0aace015eb21e8c7b7a691623ab8def6',1,'p3lib::ngt::local_file_picker']]],
+  ['handle_5fdouble_5fclick_1',['handle_double_click',['../classp3lib_1_1ngt_1_1local__file__picker.html#a3265ea408477a00ddbebe21f85103b51',1,'p3lib.ngt.local_file_picker.handle_double_click'],['../classp3lib_1_1ngt_1_1local__file__picker.html#a0aace015eb21e8c7b7a691623ab8def6',1,'p3lib.ngt.local_file_picker.handle_double_click(self, e)']]],
   ['handleoptions_2',['HandleOptions',['../classp3lib_1_1boot__manager_1_1BootManager.html#aa9d674696f3249198e314f66784d0ba5',1,'p3lib::boot_manager::BootManager']]],
   ['hash_5ffile_3',['HASH_FILE',['../classbokeh__auth__test_1_1CredentialsHasherTester.html#a635e46dba52617450c6da748a2c182d7',1,'bokeh_auth_test::CredentialsHasherTester']]],
   ['helper_2epy_4',['helper.py',['../helper_8py.html',1,'']]],
--- doc/html/search/variables_8.js
+++ doc/html/search/variables_8.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['hash_5ffile_0',['HASH_FILE',['../classbokeh__auth__test_1_1CredentialsHasherTester.html#a635e46dba52617450c6da748a2c182d7',1,'bokeh_auth_test::CredentialsHasherTester']]],
-  ['hexadecimal_5fint_5fnumber_5ftype_1',['HEXADECIMAL_INT_NUMBER_TYPE',['../classp3lib_1_1pconfig_1_1ConfigManager.html#a8692a265fefe6eba77dd0cf531eeb303',1,'p3lib::pconfig::ConfigManager']]],
-  ['host_2',['HOST',['../classtest__json__networking_1_1TestClass.html#a9753bdac85c26176cba869e2599824a3',1,'test_json_networking::TestClass']]],
-  ['hour_5fmin_5finput_5ffield_5ftype_3',['HOUR_MIN_INPUT_FIELD_TYPE',['../classp3lib_1_1ngt_1_1YesNoDialog.html#a2f3faacbf4d0727ee0f356d14e6ad6ee',1,'p3lib::ngt::YesNoDialog']]],
-  ['html_5ffile_4',['HTML_FILE',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a9f941e8f3eff218d8370ae7f8baff475',1,'p3lib::table_plot::Table2DPlotServer']]],
-  ['htmlfile_5',['htmlFile',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a53463d910a7e8f74b47f9e4745d163f4',1,'p3lib::table_plot::StaticPlotParams']]]
+  ['handle_5fdouble_5fclick_0',['handle_double_click',['../classp3lib_1_1ngt_1_1local__file__picker.html#a3265ea408477a00ddbebe21f85103b51',1,'p3lib::ngt::local_file_picker']]],
+  ['hash_5ffile_1',['HASH_FILE',['../classbokeh__auth__test_1_1CredentialsHasherTester.html#a635e46dba52617450c6da748a2c182d7',1,'bokeh_auth_test::CredentialsHasherTester']]],
+  ['hexadecimal_5fint_5fnumber_5ftype_2',['HEXADECIMAL_INT_NUMBER_TYPE',['../classp3lib_1_1pconfig_1_1ConfigManager.html#a8692a265fefe6eba77dd0cf531eeb303',1,'p3lib::pconfig::ConfigManager']]],
+  ['host_3',['HOST',['../classtest__json__networking_1_1TestClass.html#a9753bdac85c26176cba869e2599824a3',1,'test_json_networking::TestClass']]],
+  ['hour_5fmin_5finput_5ffield_5ftype_4',['HOUR_MIN_INPUT_FIELD_TYPE',['../classp3lib_1_1ngt_1_1YesNoDialog.html#a2f3faacbf4d0727ee0f356d14e6ad6ee',1,'p3lib::ngt::YesNoDialog']]],
+  ['html_5ffile_5',['HTML_FILE',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a9f941e8f3eff218d8370ae7f8baff475',1,'p3lib::table_plot::Table2DPlotServer']]],
+  ['htmlfile_6',['htmlFile',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a53463d910a7e8f74b47f9e4745d163f4',1,'p3lib::table_plot::StaticPlotParams']]]
 ];
