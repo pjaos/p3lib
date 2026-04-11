94c921c53bd0f9f6f2d5eafb06c1d953786feb23
Paul Austen <pjaos@gmail.com>
2026-04-11 10:23:04 +0100

    Bump to 1.1.158

--- doc/html/search/variables_11.js
+++ doc/html/search/variables_11.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['request_0',['request',['../classp3lib_1_1table__plot_1_1Table2DPlotServer_1_1ServerSessionHandler.html#a51e6a0d2bcb8531a5e5adcd66a77aa3b',1,'p3lib.table_plot.Table2DPlotServer.ServerSessionHandler.request'],['../classtest__json__networking_1_1ServerSessionHandler.html#a51e6a0d2bcb8531a5e5adcd66a77aa3b',1,'test_json_networking.ServerSessionHandler.request']]],
   ['response_5fdict_5fkey_1',['RESPONSE_DICT_KEY',['../classp3lib_1_1mqtt__rpc_1_1MQTTRPCClient.html#a1af0c16ea3cdf3e34ebcf6d283ef4fd0',1,'p3lib::mqtt_rpc::MQTTRPCClient']]],
-  ['result_5fdata_5ftype_2',['RESULT_DATA_TYPE',['../classp3lib_1_1table__plot_1_1GUIMessage.html#a414248ceacde6f1c9eabbd571677df7a',1,'p3lib::table_plot::GUIMessage']]],
-  ['result_5ftitle_3',['RESULT_TITLE',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a7865bb410acf9adefc592ff7adf8bc2e',1,'p3lib::table_plot::Table2DPlotServer']]],
-  ['result_5fwidth_4',['RESULT_WIDTH',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a10663d2637062cc5af25ce8ffadb6b9d',1,'p3lib::table_plot::Table2DPlotServer']]],
-  ['resulttitle_5',['resultTitle',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a4539a8bafabafbb1f1b29379e7975693',1,'p3lib::table_plot::StaticPlotParams']]],
-  ['resultwidth_6',['resultWidth',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a41bf2776d30fbfa53f75d311588bebd5',1,'p3lib::table_plot::StaticPlotParams']]],
-  ['root_5fservice_5ffolder_7',['ROOT_SERVICE_FOLDER',['../classp3lib_1_1boot__manager_1_1LinuxBootManager.html#a1e8fc68236ab16d934acd54566c3d011',1,'p3lib::boot_manager::LinuxBootManager']]],
-  ['rx_5fbuffer_5fsize_8',['RX_BUFFER_SIZE',['../classp3lib_1_1ssh_1_1SSHTunnelManager.html#a6a3f56dbe4f7361619f2460bae197f03',1,'p3lib::ssh::SSHTunnelManager']]]
+  ['restart_5fcmd_5fopt_2',['RESTART_CMD_OPT',['../classp3lib_1_1boot__manager_1_1BootManager.html#a697a5de90a17dc54b47bd7c463861478',1,'p3lib::boot_manager::BootManager']]],
+  ['result_5fdata_5ftype_3',['RESULT_DATA_TYPE',['../classp3lib_1_1table__plot_1_1GUIMessage.html#a414248ceacde6f1c9eabbd571677df7a',1,'p3lib::table_plot::GUIMessage']]],
+  ['result_5ftitle_4',['RESULT_TITLE',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a7865bb410acf9adefc592ff7adf8bc2e',1,'p3lib::table_plot::Table2DPlotServer']]],
+  ['result_5fwidth_5',['RESULT_WIDTH',['../classp3lib_1_1table__plot_1_1Table2DPlotServer.html#a10663d2637062cc5af25ce8ffadb6b9d',1,'p3lib::table_plot::Table2DPlotServer']]],
+  ['resulttitle_6',['resultTitle',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a4539a8bafabafbb1f1b29379e7975693',1,'p3lib::table_plot::StaticPlotParams']]],
+  ['resultwidth_7',['resultWidth',['../classp3lib_1_1table__plot_1_1StaticPlotParams.html#a41bf2776d30fbfa53f75d311588bebd5',1,'p3lib::table_plot::StaticPlotParams']]],
+  ['root_5fservice_5ffolder_8',['ROOT_SERVICE_FOLDER',['../classp3lib_1_1boot__manager_1_1LinuxBootManager.html#a1e8fc68236ab16d934acd54566c3d011',1,'p3lib::boot_manager::LinuxBootManager']]],
+  ['rx_5fbuffer_5fsize_9',['RX_BUFFER_SIZE',['../classp3lib_1_1ssh_1_1SSHTunnelManager.html#a6a3f56dbe4f7361619f2460bae197f03',1,'p3lib::ssh::SSHTunnelManager']]]
 ];
